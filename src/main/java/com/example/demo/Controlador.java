6253f3c017621ec48c1fc29010670ed8d0c264a6
Ovs <ovanegas@gmail.com>
2022-09-15 23:57:10 -0500

    version1

---
+++ src/main/java/com/example/demo/Controlador.java
@@ -0,0 +1,16 @@
+
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controlador {
+    
+    @GetMapping("/")
+    public String mensaje(){
+        System.out.println("mensaje de exito");
+        return "Hola, esta es una prueba con Spring Boot con G35 ombe";
+    }
+    
+}
