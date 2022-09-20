e23ab22d3577013695614589d34a10088b6559fa
ovanesu <113501991+ovanesu@users.noreply.github.com>
2022-09-20 00:04:14 -0500

    Update Controlador.java

--- src/main/java/com/example/demo/Controlador.java
+++ src/main/java/com/example/demo/Controlador.java
@@ -10,7 +10,7 @@ public class Controlador {
     @GetMapping("/")
     public String mensaje(){
         System.out.println("mensaje de exito");
-        return "Hola, esta es una prueba con Spring Boot en la nube";
+        return "Hola, esta es una prueba con Spring Boot en cloud";
     }
     
 }
